aad80add17f2c3622c45cd54006f3642a3e44afe
EUNHEE JANG <6810779s@naver.com>
2021-07-27 23:26:13 +0900

    media js reload

--- js/info.js
+++ js/info.js
@@ -4,6 +4,23 @@ let info_text = document.querySelector('.main_name_info h1');
 let TopNum_info_big = 400;
 let direction_info = '';
 
+window.onresize = function(){
+  document.location.reload();
+}; //브라우저 창 크기 조절시 자동 새로고침
+
+if (matchMedia("screen and (min-width: 335px) and (max-width: 757px)").matches){
+  TopNum_info_big = 120;
+  direction_info = ''
+}else if(matchMedia("screen and (min-width: 758px) and (max-width: 1023px)").matches){
+  
+  TopNum_info_big = 280;
+  direction_info = ''
+}else{
+  
+  TopNum_info_big = 400;
+  direction_info = ''
+}
+
 document.addEventListener('scroll', function(){
   let currentPos = document.documentElement.scrollTop;
   if (currentPos>TopNum_info_big){
@@ -16,11 +33,13 @@ document.addEventListener('scroll', function(){
   }else{
     if(direction_info!='up'){
       direction_info = 'up';
-      info__contact.style.top = 35 +'rem' ;
-      info__smallImg.style.top = 35 + 'rem';
+      info__contact.style.top = 100 +'%' ;
+      info__smallImg.style.top = 100 + '%';
       info_text.innerHTML = 'Information';
     }
   }
 });
+
+
 
 
