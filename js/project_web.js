df27ac74d021298df8ae2aeab5357c024c8813df
EUNHEE JANG <6810779s@naver.com>
2021-07-27 11:41:09 +0900

    add project

--- js/project_web.js
+++ js/project_web.js
@@ -6,6 +6,10 @@ let cafe_project_img = document.querySelector('.cafe .cafe_project_img');
 let cafe_up = document.querySelector('.cafe .up');
 let cafe_down = document.querySelector('.cafe .down');
 
+let portfolioSecond_project_img = document.querySelector('.portfolio_second .portfolioSecond_project_img');
+let portfolioSecond__up = document.querySelector('.portfolio_second .up');
+let portfolioSecond__down = document.querySelector('.portfolio_second .down');
+
 portfolio_up.addEventListener('click',function(){
   portfolio_project_img.style.top = (-293.8 + 40) + 'rem';
 });
@@ -22,6 +26,13 @@ cafe_down.addEventListener('click',function(){
   cafe_project_img.style.top = 0;
 });
 
+portfolioSecond__up.addEventListener('click',function(){
+  portfolioSecond_project_img.style.top = (-341.8 + 40) + 'rem';
+});
+
+portfolioSecond__down.addEventListener('click',function(){
+  portfolioSecond_project_img.style.top = 0;
+});
 
 
 
