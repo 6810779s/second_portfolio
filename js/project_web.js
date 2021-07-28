2495e9370d57731863fc07d24caa8d5ddf2286e4
EUNHEE JANG <6810779s@naver.com>
2021-07-29 00:50:43 +0900

    media query

--- js/project_web.js
+++ js/project_web.js
@@ -11,32 +11,37 @@ let portfolioSecond__up = document.querySelector('.portfolio_second .up');
 let portfolioSecond__down = document.querySelector('.portfolio_second .down');
 
 let portfolio_height = (portfolio_project_img.clientHeight)/10;
+console.log(portfolio_height);
 let cafe_height = (cafe_project_img.clientHeight)/10;
 let portfolioSecond_height = (portfolioSecond_project_img.clientHeight)/10;
+let container_height = (document.querySelector('.portfolio .portfolio_container').clientHeight)/10;
 
+console.log(container_height);
 
     portfolio_up.addEventListener('click',function(){
     // portfolio_project_img.style.top = (-293.8 + 40) + 'rem';
-    portfolio_project_img.style.top = -(portfolio_height) + 40 + 'rem';
+    portfolio_project_img.style.top = 0;
     });
     portfolio_down.addEventListener('click',function(){
-    portfolio_project_img.style.top = 0;
+      portfolio_project_img.style.top = -(portfolio_height) + container_height + 'rem';
   });
   
   cafe_up.addEventListener('click',function(){
-    cafe_project_img.style.top = -(cafe_height) + 40 + 'rem';
+    cafe_project_img.style.top = 0;
+    
   });
   
   cafe_down.addEventListener('click',function(){
-    cafe_project_img.style.top = 0;
+    cafe_project_img.style.top = -(cafe_height) + container_height + 'rem';
   });
   
   portfolioSecond__up.addEventListener('click',function(){
-    portfolioSecond_project_img.style.top = -(portfolioSecond_height) + 40 + 'rem';
+    portfolioSecond_project_img.style.top = 0;
+    
   });
   
   portfolioSecond__down.addEventListener('click',function(){
-    portfolioSecond_project_img.style.top = 0;
+    portfolioSecond_project_img.style.top = -(portfolioSecond_height) + container_height + 'rem';
   });
 
 
