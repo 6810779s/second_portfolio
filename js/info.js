e29168b922bd618fa2300638ddc65e2af3a10453
EUNHEE JANG <6810779s@naver.com>
2021-08-13 16:26:52 +0900

    refactor

--- js/info.js
+++ js/info.js
@@ -1,66 +1,60 @@
-let info__contact = document.querySelector('#info .info__bottomContainer .info__contact');
-let info__smallImg = document.querySelector('#info .info__img1 .info__img2');
-let info_text = document.querySelector('.main_name_info h1');
-let info_next_btn = document.querySelector('.infoMe__writing .info_mobile_btn');
+let info__contact = document.querySelector(
+  "#info .info__bottomContainer .info__contact"
+);
+let info__smallImg = document.querySelector("#info .info__img1 .info__img2");
+let info_text = document.querySelector(".main_name_info h1");
+let info_next_btn = document.querySelector(".info__me .info_mobile_btn");
 let TopNum_info_big = 500;
-let direction_info = '';
+let direction_info = "";
 
-window.onload = function(){ 
-  if (location.href.indexOf('reloaded')==-1) location.replace(location.href+'?reloaded'); 
-  //if (parent.location.href.indexOf('reloaded')==-1) parent.location.replace(parent.location.href+'?reloaded'); 
-} 
+window.onload = function () {
+  if (location.href.indexOf("reloaded") == -1)
+    location.replace(location.href + "?reloaded");
+  //if (parent.location.href.indexOf('reloaded')==-1) parent.location.replace(parent.location.href+'?reloaded');
+};
 // window.onresize = function(){
 //   document.location.reload();
 // }; //브라우저 창 크기 조절시 자동 새로고침
 
-if (matchMedia("screen and(max-width: 767px)").matches){
-  
-}else if(matchMedia("screen and (max-width: 1023px)").matches){
-}else if(matchMedia("screen and (min-width: 1024px)").matches){
-  
+if (matchMedia("screen and(max-width: 767px)").matches) {
+} else if (matchMedia("screen and (max-width: 1023px)").matches) {
+} else if (matchMedia("screen and (min-width: 1024px)").matches) {
   TopNum_info_big = 500;
-  direction_info = ''
-  document.addEventListener('scroll', function(){
+  direction_info = "";
+  document.addEventListener("scroll", function () {
     let currentPos = document.documentElement.scrollTop;
-    if (currentPos>TopNum_info_big){
-      if(direction_info!='down'){
-        direction_info='down';
-        info__contact.style.top = 0 ;
+    if (currentPos > TopNum_info_big) {
+      if (direction_info != "down") {
+        direction_info = "down";
+        info__contact.style.top = 0;
         info__smallImg.style.top = 0;
-        info_text.innerHTML = 'Information';
+        info_text.innerHTML = "Information";
       }
-    }else{
-      if(direction_info!='up'){
-        direction_info = 'up';
-        info__contact.style.top = 100 +'%' ;
-        info__smallImg.style.top = 100 + '%';
-        info_text.innerHTML = 'About Me';
+    } else {
+      if (direction_info != "up") {
+        direction_info = "up";
+        info__contact.style.top = 100 + "%";
+        info__smallImg.style.top = 100 + "%";
+        info_text.innerHTML = "About Me";
       }
     }
   });
 }
 
-info_next_btn.addEventListener('click', function(){
-  if (info_next_btn.value==='next'){
+info_next_btn.addEventListener("click", function () {
+  if (info_next_btn.value === "next") {
     info__contact.style.top = 0;
     info__smallImg.style.top = 0;
-    info_next_btn.value='prev';
-    info_next_btn.innerHTML='prev';
-    info_text.innerHTML = 'Information';
+    info_next_btn.value = "prev";
+    info_next_btn.innerHTML = "prev";
+    info_text.innerHTML = "Information";
     info_next_btn.style.background = "rgb(250, 221, 221)";
-  }else{
-    info__contact.style.top = 100 +'%' ;
-    info__smallImg.style.top = 100+ '%';
-    info_next_btn.value='next';
-    info_next_btn.innerHTML='next';
-    info_text.innerHTML = 'About Me';
+  } else {
+    info__contact.style.top = 100 + "%";
+    info__smallImg.style.top = 100 + "%";
+    info_next_btn.value = "next";
+    info_next_btn.innerHTML = "next";
+    info_text.innerHTML = "About Me";
     info_next_btn.style.background = "rgb(241, 151, 150)";
   }
 });
-
-
-
-
-
-
-
